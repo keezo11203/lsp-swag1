c4632d624cde8741cbe8175c99d8f2c5dbf95133
keezo11203 <kaitlynellis0813@gmail.com>
2023-02-06 20:34:22 -0500

    files again

--- lsp1/src/org/howard/edu/hw2/FileReader.java
+++ lsp1/src/org/howard/edu/hw2/FileReader.java
@@ -1,29 +1,33 @@
 package org.howard.edu.hw2;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.net.URL;
-import java.util.Scanner;
+import java.util.LinkedHashMap;
 
 public class FileReader {
-	public String readToString(String resource) throws FileNotFoundException {
-		URL url = getClass().getClassLoader().getResource(resource);
-		
-		if (url != null) {
-			File file = new File(url.getPath());
-			
-			Scanner sc = null;
-			try {
-				sc = new Scanner(file);
-				String output = "";
-				while (sc.hasNextLine()) {
-					output = output + sc.nextLine() + " ";
-				}
-				return output.trim();
-			} finally {
-				sc.close();
-			}
+
+    public static void main(String[] args) {
+        FileReader fr = new FileReader();
+        LinkedHashMap<String, Integer> my_map = new LinkedHashMap<String, Integer>(); //I decided to use a hash map because it is the most similar to a dictionary in python
+
+        String file_string = fr.readToString("words.txt"); //store the text from the file in a string
+		String[] separated = file_string.toLowerCase().split(" "); //put each word into an array removing all punctuation and spaces
+		for (int x = 0; x < separated.length; x++) {
+		    if (separated[x].length() > 3) {
+		        if (my_map.get(separated[x]) == null) { //if it is not in the hash map, add it and set its value to zero
+		            my_map.put(separated[x], 1);
+		        }
+		        else { //if it is in the hash map increment its count
+		            my_map.put(separated[x], my_map.get(separated[x]) + 1);
+		        }
+		    }
 		}
-		throw new FileNotFoundException();
+		for (String i : my_map.keySet()) {
+		    System.out.println(i + " " + my_map.get(i));
+		}
+
+    }
+
+	String readToString(String string) {
+		return null;
 	}
+
 }
